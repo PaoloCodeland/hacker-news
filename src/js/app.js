89bf96d7143af98e6a73c50719127dc812a2957b
PaoloDuzioni <paolo'duzioni@gmail.com>
2022-10-06 17:07:00 +0200

    add Header component

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,33 @@
+import Header from './components/Header';
 import toggleTheme from './functions/toggleTheme';
 import RouterHandler from './router';
 
 class App {
     constructor() {
         // Refs
-        this.themeBtn = document.querySelector('.theme-btn');
+        this.$app = document.querySelector('#app');
+        this.$themeBtn = null;
 
         // Init
+        this.RenderGlobals();
         RouterHandler();
         this.eventHandlers();
     }
 
+    RenderGlobals() {
+        const parser = new DOMParser();
+        // Header
+        const header = parser.parseFromString(Header(), 'text/html').body
+            .childNodes[0];
+        this.$app.prepend(header);
+
+        this.$themeBtn = document.querySelector('.theme-btn');
+    }
+
     // Events
     eventHandlers() {
-        this.themeBtn.addEventListener('click', () =>
-            toggleTheme(this.themeBtn)
+        this.$themeBtn.addEventListener('click', () =>
+            toggleTheme(this.$themeBtn)
         );
     }
 }
